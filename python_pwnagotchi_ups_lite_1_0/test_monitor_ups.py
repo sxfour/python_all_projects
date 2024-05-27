e0e6ee94bf5c694092e26730940d4e3e7e6031ed
Egor Levashov <levashoveg@yandex.ru>
2024-05-27 11:55:53 +0300

    Update test_monitor_ups.py

--- python_pwnagotchi_ups_lite_1_0/test_monitor_ups.py
+++ python_pwnagotchi_ups_lite_1_0/test_monitor_ups.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 # this i2c used ups 1.0
-ADDRESS = 0x72
+ADDRESS = int(''.join(filter(str.isdigit, str(subprocess.run(["sudo", "i2cdetect", "-y", "1"], capture_output=True)).split("70:")[1])), 16)
 
 
 def readVoltage(bus):
@@ -37,13 +37,11 @@ def readCapacity(bus):
 
 
 def QuickStart(bus):
-    address = 0x72
-    bus.write_word_data(address, 0x06, 0x4000)
+    bus.write_word_data(ADDRESS, 0x06, 0x4000)
 
 
 def PowerOnReset(bus):
-    address = 0x72
-    bus.write_word_data(address, 0xfe, 0x0054)
+    bus.write_word_data(ADDRESS, 0xfe, 0x0054)
 
 
 bus = smbus2.SMBus(1)
