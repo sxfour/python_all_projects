aadb0d8d6ab132d679335f626d35bf882bce5e7c
Egor Levashov <levashoveg@yandex.ru>
2024-06-25 09:33:59 +0300

    Update tsrv_table.py

--- python_opc_read_tsrv/tsrv_table.py
+++ python_opc_read_tsrv/tsrv_table.py
@@ -14,14 +14,14 @@ from logging import *
 class OPCResponse:
     def __init__(self, url, device):
         self.url = url
-        self.title = ""
+        self.title = "Опрос Взлет ТСР-024М (Пикет 1)"
         self.client = Client(self.url)
         self.device = device
 
     def get_data_opc(self, sleep_conn):
         while True:
             table = Table(
-                "Полный тег", "Тег", "Значение", "Время", box=box.MINIMAL, title=self.title
+                "Полный тег", "Комментарий", "Тег", "Значение", "Время", box=box.MINIMAL, title=self.title
             )
 
             # table.width = 120
@@ -29,9 +29,11 @@ class OPCResponse:
             table.columns[0].style = "spring_green2"
             table.header_style = "bright_cyan"
             table.columns[1].style = "spring_green2"
-            table.columns[1].justify = "right"
-            table.columns[2].style = "light_goldenrod1"
-            table.columns[3].style = "light_cyan1"
+            table.columns[1].justify = "center"
+            table.columns[2].justify = "right"
+            table.columns[2].style = "light_cyan1"
+            table.columns[3].style = "light_goldenrod1"
+            table.columns[4].style = "light_cyan1"
 
             try:
                 self.client.connect()
@@ -46,9 +48,11 @@ class OPCResponse:
                         for value_node in self.device:
                             var = self.device.get(value_node)
                             node = self.client.get_node(var).get_value()
+                            descr = self.client.get_node(var).get_description().Text
                             data_to_restapi[var] = round(node, 2)
 
-                            table.add_row(str(var.partition(".")[2]), str(value_node), str(round(node, 4)), f_time)
+                            table.add_row(str(var.partition(".")[2]), descr, str(value_node), str(round(node, 4)), f_time)
+                            # table.add_row(descr, str(value_node), str(round(node, 4)), f_time)
 
                         data_to_restapi.clear()
 
@@ -91,7 +95,7 @@ if __name__ == "__main__":
     with Live(console=console, screen=True, auto_refresh=False) as live:
         while True:
             try:
-                live.update(OPCResponse("", device=TSRV_022).get_data_opc(sleep_conn=1), refresh=True)
+                live.update(OPCResponse("opc.tcp://192.168.0.120:55000", device=TSRV_024).get_data_opc(sleep_conn=1), refresh=True)
             except Exception as ex:
                 gtime = strftime("%d-%m-%Y %H:%M:%S", localtime(time()))
 
