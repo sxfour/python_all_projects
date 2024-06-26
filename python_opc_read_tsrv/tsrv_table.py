c36ae5a853c814ee01d1bef4ecdc632a2955267e
Egor Levashov <levashoveg@yandex.ru>
2024-06-26 09:58:52 +0300

    Update tsrv_table.py

--- python_opc_read_tsrv/tsrv_table.py
+++ python_opc_read_tsrv/tsrv_table.py
@@ -5,6 +5,8 @@ from rich.table import Table
 from rich.live import Live
 from rich import box
 
+from os import system
+
 from opcua import Client
 from vzljot_tsrv import *
 
@@ -12,72 +14,67 @@ from logging import *
 
 
 class OPCResponse:
-    def __init__(self, url, device):
+    def __init__(self, url, device, title):
         self.url = url
-        self.title = ""
+        self.title = title
         self.client = Client(self.url)
         self.device = device
 
-    def get_data_opc(self, sleep_conn):
+    def get_data_opc(self):
         while True:
             table = Table(
-                "Полный тег", "Комментарий", "Тег", "Значение", "Время", box=box.MINIMAL, title=self.title
+                "Тег", "Комментарий", "Значение", "Время запроса", box=box.SQUARE, title=self.title
             )
 
             # table.width = 120
             table.border_style = "bright_cyan"
             table.columns[0].style = "spring_green2"
+            table.columns[0].justify = "right"
             table.header_style = "bright_cyan"
             table.columns[1].style = "spring_green2"
-            table.columns[1].justify = "center"
-            table.columns[2].justify = "right"
-            table.columns[2].style = "light_cyan1"
-            table.columns[3].style = "light_goldenrod1"
-            table.columns[4].style = "light_cyan1"
+            table.columns[1].justify = "right"
+            table.columns[2].justify = "left"
+            table.columns[2].style = "light_goldenrod1"
+            table.columns[3].style = "light_cyan1"
 
             try:
                 self.client.connect()
 
-                sleep(sleep_conn)
-
                 while True:
                     try:
-                        f_time = strftime("%d.%m.%Y / %H:%M:%S", localtime(time()))
-                        data_to_restapi = dict()
+                        # data_to_restapi = dict()
 
                         for value_node in self.device:
                             var = self.device.get(value_node)
                             node = self.client.get_node(var).get_value()
                             descr = self.client.get_node(var).get_description().Text
-                            data_to_restapi[var] = round(node, 2)
+                            # data_to_restapi[var] = round(node, 2)
 
-                            table.add_row(str(var.partition(".")[2]), descr, str(value_node), str(round(node, 4)), f_time)
-                            # table.add_row(descr, str(value_node), str(round(node, 4)), f_time)
+                            f_time = strftime("%d.%m.%Y / %H:%M:%S", localtime(time()))
+                            table.add_row(str(value_node), descr, str(round(node, 4)), f_time)
 
-                        data_to_restapi.clear()
+                        # data_to_restapi.clear()
 
                         self.disconnect()
-
                         return table
 
                     except Exception as ex_:
                         error(ex_)
+
                         break
 
-            except (
-                    ConnectionRefusedError,
-                    ConnectionAbortedError,
-                    ConnectionResetError,
-                    ConnectionError
-            ) as conn_err:
-                error(conn_err)
+                    finally:
+                        system('cls')
 
-                print("[{0}] ERROR {1}.".format(strftime("%d-%m-%Y %H:%M:%S", localtime(time())), conn_err))
+            except Exception as ex__:
+                error(ex__)
 
     def disconnect(self):
         try:
             self.client.close_session()
             self.client.close_secure_channel()
+        except Exception as err:
+            error(err)
         finally:
             self.client.disconnect_socket()
 
@@ -95,13 +92,15 @@ if __name__ == "__main__":
     with Live(console=console, screen=True, auto_refresh=False) as live:
         while True:
             try:
-                live.update(OPCResponse("", device=TSRV_024).get_data_opc(sleep_conn=1), refresh=True)
+                live.update(OPCResponse(
+                    url="opc.tcp://192.168.0.124:55000",
+                    device=TSRV_022,
+                    title="Опрос Взлет ТСР-022 (33км)"
+                ).get_data_opc(), refresh=True)
             except Exception as ex:
                 gtime = strftime("%d-%m-%Y %H:%M:%S", localtime(time()))
 
                 error(ex)
-
-                print("[{0}] ERROR {1}, sleep 10 seconds...".format(gtime, ex))
 
                 sleep(retry_conn)
                 continue
