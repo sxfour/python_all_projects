16d362269e1e43b2cd2dc69fc9b9a250a6a94b30
Egor Levashov <levashoveg@yandex.ru>
2024-06-27 20:33:46 +0300

    Update tsrv_table.py

--- python_opc_read_tsrv/tsrv_table.py
+++ python_opc_read_tsrv/tsrv_table.py
@@ -20,54 +20,41 @@ class OPCResponse:
         self.client = Client(self.url)
         self.device = device
 
+        self.client.connect()
+
     def get_data_opc(self):
-        while True:
-            table = Table(
-                "Тег", "Комментарий", "Значение", "Время запроса", box=box.SQUARE, title=self.title
-            )
+        table = Table(
+            "Тег", "Комментарий", "Значение", "Время запроса", box=box.SQUARE, title=self.title
+        )
 
-            # table.width = 120
-            table.border_style = "bright_cyan"
-            table.columns[0].style = "spring_green2"
-            table.columns[0].justify = "right"
-            table.header_style = "bright_cyan"
-            table.columns[1].style = "spring_green2"
-            table.columns[1].justify = "right"
-            table.columns[2].justify = "left"
-            table.columns[2].style = "light_goldenrod1"
-            table.columns[3].style = "light_cyan1"
+        # table.width = 120
+        table.border_style = "bright_cyan"
+        table.columns[0].style = "spring_green2"
+        table.columns[0].justify = "right"
+        table.header_style = "bright_cyan"
+        table.columns[1].style = "spring_green2"
+        table.columns[1].justify = "right"
+        table.columns[2].justify = "left"
+        table.columns[2].style = "light_goldenrod1"
+        table.columns[3].style = "light_cyan1"
 
-            try:
-                self.client.connect()
+        try:
+            for value_node in self.device:
+                var = self.device.get(value_node)
+                node = self.client.get_node(var).get_value()
+                descr = self.client.get_node(var).get_description().Text
 
-                while True:
-                    try:
-                        # data_to_restapi = dict()
+                f_time = strftime("%d.%m.%Y / %H:%M:%S", localtime(time()))
+                table.add_row(str(value_node), descr, str(round(node, 4)), f_time)
 
-                        for value_node in self.device:
-                            var = self.device.get(value_node)
-                            node = self.client.get_node(var).get_value()
-                            descr = self.client.get_node(var).get_description().Text
-                            # data_to_restapi[var] = round(node, 2)
+            return table
 
-                            f_time = strftime("%d.%m.%Y / %H:%M:%S", localtime(time()))
-                            table.add_row(str(value_node), descr, str(round(node, 4)), f_time)
+        except Exception as ex__:
+            error(ex__)
 
-                        # data_to_restapi.clear()
-
-                        self.disconnect()
-                        return table
-
-                    except Exception as ex_:
-                        error(ex_)
-
-                        break
-
-                    finally:
-                        system('cls')
-
-            except Exception as ex__:
-                error(ex__)
+        finally:
+            system('cls')
+            self.disconnect()
 
     def disconnect(self):
         try:
@@ -93,9 +80,9 @@ if __name__ == "__main__":
         while True:
             try:
                 live.update(OPCResponse(
-                    url="ВАШ ОПС СЕРВЕР",
-                    device=TSRV_022,
-                    title="Название"
+                    url="opc.tcp://",
+                    device=TSRV_024,
+                    title=""
                 ).get_data_opc(), refresh=True)
             except Exception as ex:
                 gtime = strftime("%d-%m-%Y %H:%M:%S", localtime(time()))
