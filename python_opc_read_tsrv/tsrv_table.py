e31e3a89f71256bbcb52e6c0c6180c0e3933c7e5
Egor Levashov <levashoveg@yandex.ru>
2024-06-29 23:31:08 +0300

    Update tsrv_table.py

--- python_opc_read_tsrv/tsrv_table.py
+++ python_opc_read_tsrv/tsrv_table.py
@@ -12,6 +12,12 @@ from vzljot_tsrv import *
 
 from logging import *
 
+# ASCII color codes
+green = '\033[32;3m'
+yellow = '\033[33;3m'
+fail = '\x1b[31;3m'
+end = '\033[0m'
+
 
 class OPCResponse:
     def __init__(self, url, device, title):
@@ -22,7 +28,7 @@ class OPCResponse:
 
         self.client.connect()
 
-    def get_data_opc(self):
+    def get_data_opc(self, last_vals):
         table = Table(
             "Тег", "Комментарий", "Значение", "Время запроса", box=box.SQUARE, title=self.title
         )
@@ -38,14 +44,30 @@ class OPCResponse:
         table.columns[2].style = "light_goldenrod1"
         table.columns[3].style = "light_cyan1"
 
+        # now_vals = list()
+
         try:
+            counter = 0
             for value_node in self.device:
                 var = self.device.get(value_node)
-                node = self.client.get_node(var).get_value()
+                node = round(self.client.get_node(var).get_value(), 4)
                 descr = self.client.get_node(var).get_description().Text
 
                 f_time = strftime("%d.%m.%Y / %H:%M:%S", localtime(time()))
-                table.add_row(str(value_node), descr, str(round(node, 4)), f_time)
+
+                # table.add_row(str(value_node), descr, str(round(node, 4)), f_time, end_section=)
+
+                last_vals.append(node)
+                # now_vals.append(round(node, 4))
+
+                if node == last_vals[counter]:
+                    table.add_row(str(value_node), descr, str(node), f_time)
+                elif node < last_vals[counter]:
+                    table.add_row(str(value_node), descr, str(node), f_time, style="on red")
+                elif node > last_vals[counter]:
+                    table.add_row(str(value_node), descr, str(node), f_time, style="on blue")
+
+                counter += 1
 
             return table
 
@@ -54,6 +76,9 @@ class OPCResponse:
 
         finally:
             system('cls')
+
+            # now_vals.clear()
+
             self.disconnect()
 
     def disconnect(self):
@@ -77,13 +102,19 @@ if __name__ == "__main__":
     retry_conn = 10
 
     with Live(console=console, screen=True, auto_refresh=False) as live:
+        last_values = list()
         while True:
             try:
+                if len(last_values) > 12:
+                    last_values.clear()
+
                 live.update(OPCResponse(
                     url="opc.tcp://",
-                    device=TSRV_024,
+                    device=TSRV_022,
                     title=""
-                ).get_data_opc(), refresh=True)
+                ).get_data_opc(last_values), refresh=True)
+
+                # print(last_values)
             except Exception as ex:
                 gtime = strftime("%d-%m-%Y %H:%M:%S", localtime(time()))
 
